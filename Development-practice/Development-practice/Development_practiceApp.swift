b23a4bfe55f9558deca8fed110792ea3e77be45b
Yuz727 <unikapriv@gmail.com>
2025-11-30 12:56:40 +0900

    Add : XcodeでのPJ作成時生成物

---
+++ Development-practice/Development-practice/Development_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Development_practiceApp.swift
+//  Development-practice
+//
+//  Created by 宇都佑太 on 2025/11/30.
+//
+
+import SwiftUI
+
+@main
+struct Development_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
